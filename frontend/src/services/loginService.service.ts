5531c43724d3f5ea90a0fa87ca044e344b90adec
Michael Rafael Franco Rodriguez <108529460+devmfranco@users.noreply.github.com>
2023-12-20 12:54:53 -0400

    Cambios

---
+++ frontend/src/services/loginService.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { createClient } from '@supabase/supabase-js';
+import { environment } from '../environments/environment.development';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginServiceService {
+constructor() {
+
+}
+}
+
+
