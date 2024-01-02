9468a06d7b1a0bf9c498d80ce5413047000bd58b
Michael Rafael Franco Rodriguez <108529460+devmfranco@users.noreply.github.com>
2024-01-02 09:48:25 -0400

    Cambios

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -10,6 +10,6 @@ import { getAuth, provideAuth } from '@angular/fire/auth';
 
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"fitplans","appId":"1:185348207581:web:7cc6049998fe5248f8fc34","databaseURL":"https://fitplans-default-rtdb.firebaseio.com","storageBucket":"fitplans.appspot.com","apiKey":"AIzaSyCBkmatsfMqG1Z08LFiFOSKTHrUUFR_OsQ","authDomain":"fitplans.firebaseapp.com","messagingSenderId":"185348207581"}))), importProvidersFrom(provideAuth(() => getAuth()))]
+  providers: [provideRouter(routes), provideClientHydration(), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"fitplans","appId":"1:185348207581:web:7cc6049998fe5248f8fc34","databaseURL":"https://fitplans-default-rtdb.firebaseio.com","storageBucket":"fitplans.appspot.com","apiKey":"AIzaSyCBkmatsfMqG1Z08LFiFOSKTHrUUFR_OsQ","authDomain":"fitplans.firebaseapp.com","messagingSenderId":"185348207581"}))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"fitplans","appId":"1:185348207581:web:7cc6049998fe5248f8fc34","databaseURL":"https://fitplans-default-rtdb.firebaseio.com","storageBucket":"fitplans.appspot.com","apiKey":"AIzaSyCBkmatsfMqG1Z08LFiFOSKTHrUUFR_OsQ","authDomain":"fitplans.firebaseapp.com","messagingSenderId":"185348207581"}))), importProvidersFrom(provideAuth(() => getAuth()))]
 };
 
